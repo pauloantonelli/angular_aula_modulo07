50a6071db7bef0dd6d93e03fb1149cb16f5baeb8
pauloantonelli <pauloantonelli@hotmail.com>
2019-05-24 13:35:43 -0300

    fim da aula

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   public usuario = {
-    nome: 'Paulo',
-    email: 'paulo@paulo.com'
+    nome: null,
+    email: null
   }
   constructor() { }
 
