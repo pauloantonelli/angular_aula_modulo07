ac4891317753e5b3b712fc6a3ebe1ab63411dd99
pauloantonelli <pauloantonelli@hotmail.com>
2019-05-25 10:43:42 -0300

    fim da aula

--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { TemplateFormComponent } from './template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent,
   ],
   imports: [
     CommonModule,
