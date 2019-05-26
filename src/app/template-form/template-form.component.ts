f7c6f2b1049dfcd1134edb0faf187c5de06b0330
pauloantonelli <pauloantonelli@hotmail.com>
2019-05-26 11:38:38 -0300

    fim da aula

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
@@ -11,7 +15,7 @@ export class TemplateFormComponent implements OnInit {
     nome: null,
     email: null
   }
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
@@ -28,5 +32,47 @@ export class TemplateFormComponent implements OnInit {
       'has-error': this.verificaValidoTocado(campo),
       'has-feedback' : this.verificaValidoTocado(campo),
     };
+  }
+
+  limpaCampoCep() {
+    document.getElementById('cep')['value'] = '';
+    document.getElementById('complemento')['value'] = '';
+    document.getElementById('rua')['value'] = '';
+    document.getElementById('bairro')['value'] = '';
+    document.getElementById('cidade')['value'] = '';
+    document.getElementById('estado')['value'] = '';
+  }
+  preencheCamposCallback(dados) {
+    if (!dados.erro) {
+      document.getElementById('complemento')['value'] = dados.complemento;
+      document.getElementById('rua')['value'] = dados.logradouro;
+      document.getElementById('bairro')['value'] = dados.bairro;
+      document.getElementById('cidade')['value'] = dados.localidade;
+      document.getElementById('estado')['value'] = dados.uf;
+    } else {
+      this.limpaCampoCep();
+      alert('CEP não encontrado! Por favor tente novamente');
+    }
+  }
+  consultaCep(cep) {
+    const cepLimpo = cep.replace(/\D/g, '');
+
+    if (cepLimpo != '') {
+      const validaCep = /^[0-9]{8}$/;
+
+      if (validaCep.test(cepLimpo)) {
+        this.http.get('https://viacep.com.br/ws/' + cepLimpo + '/json')
+        .subscribe(
+          (cep) => {
+            this.preencheCamposCallback(cep);
+          }
+        );
+      } else {
+        this.limpaCampoCep();
+        alert('Formato de CEP incorreto, tente novamente!');
+      }
+    } else {
+      this.limpaCampoCep();
+    }
   }
 }
--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 
@@ -14,6 +15,7 @@ import { FormDebugComponent } from '../form-debug/form-debug.component';
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule
   ],
   exports: [
     TemplateFormComponent
