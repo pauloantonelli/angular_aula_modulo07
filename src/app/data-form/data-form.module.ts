46f2a7be6f520d43b141a8e5a0d8769849b6ad5e
pauloantonelli <pauloantonelli@hotmail.com>
2019-06-01 12:38:07 -0300

    fim da aula

--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DataFormComponent } from './data-form.component';
 
@@ -11,6 +11,7 @@ import { DataFormComponent } from './data-form.component';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     DataFormComponent,
