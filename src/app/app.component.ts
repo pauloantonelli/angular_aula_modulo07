7bd946e2d4c84f3fe4716ab872c0968fa71acf27
pauloantonelli <pauloantonelli@hotmail.com>
2019-06-10 14:50:28 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,5 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     console.log('(_(\n/_/`_____/)\n"  |      |\n   |""""""|');
-    console.log(
-      "%cMeuPortfol.io","color: #000; font-weight: bold; font-size: 30px; font-family: sans-serif;"
-    );
-    console.log(
-      "%cMeuPortfol.io","color: rgb(110, 175, 44); font-weight: bold; font-size: 35px; font-family: sans-serif;"
-    );
-    console.log(
-      "%cPowered by MeuPortfol.io","color: rgb(110, 175, 44); font-weight: bold; font-size: 20px; font-family: sans-serif;"
-    );
-    console.log(
-      "%chttps://meuportfol.io/","color: rgb(110, 175, 44); font-weight: bold; font-size: 10px; font-family: sans-serif;"
-    );
   }
 }
